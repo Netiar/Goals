bf3a4a8e7ef9b6696fa819c1a482757723d2abd8
Netiar <collins.odinga@student@moringaSchool.com>
2022-09-04 16:32:45 +0300

    add delete buttton logic

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,11 +9,11 @@ import { Goal } from '../goal';
 export class GoalComponent implements OnInit {
 
   goals: Goal[] = [
-     new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-     new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot',new Date(2019,6,9)),
-     new Goal(3,  'Get new phone case', 'Diana has her birthday coming up soon',new Date(2022,1,12)),
-     new Goal(4, 'Get dog food',  'Pupper likes expensive snacks',new Date(2019,0,18)),
-     new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+     new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2023,3,14)),
+     new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot',new Date(2024,6,9)),
+     new Goal(3,  'Get new phone case', 'Diana has her birthday coming up soon',new Date(2026,1,12)),
+     new Goal(4, 'Get dog food',  'Pupper likes expensive snacks',new Date(2027,0,18)),
+     new Goal(5,'Solve math homework','Damn Math',new Date(2025,2,14)),
      new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),  
   ];
   
@@ -21,9 +21,13 @@ export class GoalComponent implements OnInit {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   } 
 
-  completeGoal(isComplete: any, index: number){
+  deleteGoal(isComplete: any, index: number){
     if (isComplete) {
-      this.goals.splice(index,1);
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+      
+      if (toDelete){
+        this.goals.splice(index,1)
+      }
     }
   }
   
