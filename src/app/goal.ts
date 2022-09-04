6081605c2cb561e7d42ec6b5bde79b8217ba5c5c
Netiar <collins.odinga@student@moringaSchool.com>
2022-09-04 15:55:19 +0300

    add complete-date toconstructor

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -3,7 +3,7 @@ export class Goal {
       throw new Error('Method not implemented.');
     }
     showDescription: boolean = false;
-    constructor(public id: number, public name: string, public description: string) {
+    constructor(public id: number, public name: string, public description: string, public completeDate: Date) {
         this.showDescription = false;
     }
     
