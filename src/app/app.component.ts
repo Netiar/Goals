e4fc9519e60a69909c29b23364c160218dc0568e
Netiar <collins.odinga@student@moringaSchool.com>
2022-09-12 03:09:22 +0300

    rf: refactored code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { Goal } from './goal';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+ 
 })
+
+
 export class AppComponent {
   title(title: any) {
     throw new Error('Method not implemented.');
